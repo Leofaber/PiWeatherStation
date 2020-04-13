4fdf3f47cfad325ac204356d93b6d8a8ede618cf
Leonardo Baroncelli <leonardo.baroncelli26@gmail.com>
2020-04-13 15:20:29 +0200

    Bug fixes

--- front_end/js/io_utils.js
+++ front_end/js/io_utils.js
@@ -16,45 +16,43 @@
             endDate: moment(),
             timePicker: true,
             ranges: {
-               'Today': [moment(), moment().set({hours:0, minutes:0, seconds:0})],
-               'Past 24 H': [moment(), moment().subtract(24, 'hours')],
+               'Today': [moment().set({hours:0, minutes:0, seconds:0}), moment()],
+               'Past 24 H': [moment().subtract(24, 'hours'), moment()],
                'Last 7 Days': [moment().subtract(6, 'days'), moment()],
                'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-               'This Month': [moment().startOf('month'), moment().endOf('month')],
-               'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
             }
         }, cb);
         function cb(start, end){
-          $('#datarange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+          $('#datarange span').html(start.format() + ' - ' + end.format());
         }
 
     };
 
     _ioUtils.onSelect = function(callback){
       $('#datarange').on('apply.daterangepicker', function(ev, picker) {
-        dateE = picker.startDate.unix();
-        dateS = picker.endDate.unix();
+        $("#error-msg-div").hide();
+        dateS = picker.startDate.unix();
+        dateE = moment().unix(); // workaround. pcker.endDate is not updated!!
         callback(dateS, dateE)
       });
     };
 
-    _ioUtils.getData = function(dateS, dateE, callback){
+    _ioUtils.getData = function(dateS, dateE, success_callback, error_callback){
         console.log("Ajax request..")
         // call NodeJS API - batch data
-
+        data = {"dateS":dateS, "dateE":dateE};
+        console.log("data:",data);
         $.ajax({
             type: 'POST',
             url: "/piws/get_data",
             port: 5000,
-            data: {"dateS":dateS, "dateE":dateE},
+            data: data,
             success: function(result){
-              console.log("Ajax request success!")
-              callback(result)
+              success_callback(result)
             },
             error: function(jqXHR, textStatus, errorThrown){
-              console.log("Error!")
-              console.log(textStatus)
-              console.log(errorThrown)
+              console.log(errorThrown);
+              error_callback(errorThrown)
             }
 
       });
